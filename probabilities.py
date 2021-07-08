2cce4d39a7a0c202fe2cbd44b3649a488ab24793
vitimerida <81519967+vitimerida@users.noreply.github.com>
2021-07-08 18:15:00 +0200

    Add files via upload

---
+++ probabilities.py
@@ -0,0 +1,21 @@
+from numpy import random
+##### prob. three times in a row
+time = 0
+n = 0
+u = 0
+while time <= 10001:
+    x = random.randint(36)
+    n = n + 1
+    if x == 5:
+        y = random.randint(36)
+        if y ==5:
+            z = 5
+            if z ==5:
+                time = time + 1
+                a = 1/n
+                print("Prob. same number 3 times in a row")
+                u = u + (1/n)
+                n = 0
+                print("Numero de veces ejecutado el problema:", time)
+result = u/10001
+print(result)
